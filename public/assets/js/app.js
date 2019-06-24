0eff2918093eb794a5250598efb170e8ed54b663
Jon Curto <joncurto@gmail.com>
2019-06-24 12:43:03 -0400

    Handlebars Table Complete - almost done!

---
+++ public/assets/js/app.js
@@ -0,0 +1,11 @@
+var testObject = {
+    title: "TestTitle",
+    link: "www.test.test/htmltest",
+    time: Date.now(),
+    type: "TestOnlyNoType",
+    user: "TestUserSupreme!",
+    redditId: "TestIDABCDEFG",
+    note: "noNotesHere!"
+  }
+
+  
--- routes/scraperRoutes.js
+++ routes/scraperRoutes.js
@@ -19,19 +19,33 @@ router.get("/scrape/:user", async function(req, res) {
   res.send("done")
 });
 
-var testObject = {
-  here: "is",
-  a: "test",
-  object: "to",
-  play: "with"
-}
+var testObject = [{
+  title: "TestTitle1",
+  link: "www.test.test/htmltest1",
+  time: Date.now(),
+  type: "TestOnlyNoType1",
+  content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+  user: "TestUserSupreme!1",
+  redditId: "TestIDABCDEFG1",
+  note: "noNotesHere!1"
+},
+{
+  title: "TestTitle2",
+  link: "www.test.test/htmltest2",
+  time: Date.now(),
+  type: "TestOnlyNoType2",
+  content: "Lorem Ipsum is the single greatest threat. We are not - we are not keeping up with other websites. Lorem Ipsum best not make any more threats to your website. It will be met with fire and fury like the world has never seen. Does everybody know that pig named Lorem Ipsum? An ‘extremely credible source’ has called my office and told me that Barack Obama’s placeholder text is a fraud.",
+  user: "TestUserSupreme!2",
+  redditId: "TestIDABCDEFG2",
+  note: "noNotesHere!2"
+}]
 
 
 router.get("/fetch/:user", function(req,res){
   var user = req.params.user;
   db.Results.find({user: user})
   .then(function(dbResult) {
-      res.json(dbResult)
+    res.render("index", dbResult)  
   })
   .catch(function(err) {
       res.json(err);
@@ -39,8 +53,8 @@ router.get("/fetch/:user", function(req,res){
 })
 
 //Clicking the burger button in the eaten column will delete row in databse (destroy burger) and refresh the page
-router.get("/api/destroyBurger/:id", function(req,res){
-
+router.get("/testtable", function(req,res){
+  res.render("index", testObject)
 })
 
 //Clicking Add Burger button takes data from form and updates database (create burger) and then refreshes the page
